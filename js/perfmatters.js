c83e9dae915dd589cf4aaa09a313382a62c75867
Elizabeth Kipp <liz@elizabethkipp.com>
2016-05-02 14:44:28 -0500

    cleaning up script tags in html

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -12,3 +12,12 @@ function logCRP() {
 window.addEventListener("load", function(event) {
   logCRP();
 });
+
+  
+(function(w,g){w['GoogleAnalyticsObject']=g;
+w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+      ga('create', '51530108');
+      ga('send', 'pageview');
+
+   
